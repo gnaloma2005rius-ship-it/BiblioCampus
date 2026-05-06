33266873b5f3c65bcf413181b4dcace1389e1ccd
T.rei.44 <lot4416481@gmail.com>
2026-05-06 10:44:54 +0000

    Collaborateur A : ajout commentaire dans storage.py

---
+++ storage.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+DATA_FILE = "data/library.json"
+
+def charger_donnees():
+    if not os.path.exists(DATA_FILE):
+        return {"livres": [], "emprunts": []}
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def sauvegarder_donnees(donnees):
+    os.makedirs("data", exist_ok=True)
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(donnees, f, indent=4, ensure_ascii=False)
